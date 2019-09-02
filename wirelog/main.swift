0aef0aae173f1dd38080e2853457afdbc2b0d58d
Noah Desch <deschnl@icloud.com>
2019-09-02 14:15:41 -0400

    Initial Commit

---
+++ wirelog/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  wirelog
+//
+//  Created by Noah Desch on 9/2/19.
+//  Copyright © 2019 Noah Desch. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
