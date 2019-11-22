a5357824194f510f4948b7516d4b7aea85e9d7ea
Noah Desch <deschnl@icloud.com>
2019-11-22 17:41:53 -0500

    Added unit tests target. Lets see if we can get tests working against a command-line executable in Xcode11

---
+++ Wirelog Tests/Wirelog_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Wirelog_Tests.swift
+//  Wirelog Tests
+//
+//  Created by Noah Desch on 11/22/19.
+//  Copyright © 2019 Noah Desch. All rights reserved.
+//
+
+import XCTest
+
+class Wirelog_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- wirelog/ConfigParser.swift
+++ wirelog/ConfigParser.swift
@@ -9,7 +9,81 @@
 import Foundation
 import os
 import Network
+import Parser
 
+fileprivate func keywordCharacters(_ char: Character) -> Bool {
+    return char.isLetter && char.isUppercase
+}
+
+fileprivate struct StringNode: SyntaxNode {
+    var onSuccess: ((SyntaxNode) -> Void)?
+    var text: Substring
+    
+    var child: SyntaxNode? { fatalError() }
+    subscript(index: Int) -> SyntaxNode? { fatalError() }
+    
+    mutating func parse(from text: inout Substring) -> Bool {
+        text.dropWhitespace()
+        guard text.hasPrefix("\"") else { return false }
+        var tmp = text.dropFirst()
+        let contents = tmp.consume(while: {$0 != "\""})
+        if tmp.hasPrefix("\"") {
+            text = tmp.dropFirst()
+            self.text = contents
+            return true
+        }
+        self.text = ""
+        return false
+    }
+    
+    var debugDescription: String {
+        return "\"\(self.text)\""
+    }
+    
+    init() {
+        self.onSuccess = nil
+        self.text = ""
+    }
+}
+
+fileprivate struct IPAddrLiteralNode<Address>: SyntaxNode
+    where Address: IPAddress
+{
+    var onSuccess: ((SyntaxNode) -> Void)?
+    var text: Substring
+    var child: SyntaxNode? { fatalError() }
+    subscript(index: Int) -> SyntaxNode? { fatalError() }
+        
+    var address: Address?
+    
+    mutating func parse(from text: inout Substring) -> Bool {
+        text.dropWhitespace()
+        var tmp = text
+        
+        let _ = tmp.consume(while: {$0.isHexDigit || $0 == "." || $0 == ":"})
+        guard tmp.first?.isWhitespace ?? true else { return false }
+        let capture = text.base[text.startIndex...tmp.startIndex]
+        
+        if let value = Address(String(capture)) {
+            self.address = value
+            self.text = capture
+            text = tmp.dropFirst()
+            return true
+        }
+        self.address = nil
+        return false
+    }
+    
+    var debugDescription: String {
+        return String(describing: self.text)
+    }
+    
+    init() {
+        self.onSuccess = nil
+        self.address = nil
+        self.text = ""
+    }
+}
 
 /// Class for reading and retrieving log configurations
 class Configuration {
@@ -81,6 +155,9 @@ class Configuration {
         }
     }
     
+    /// Nicknames for re-naming hosts
+    static private var hostNicknames = [String: String]()
+    
     /// Log Specs for IPv4 hosts
     static private var ipv4HostSpecs = [IPv4Address: Spec]()
     
@@ -115,45 +192,107 @@ class Configuration {
         }
     }
     
+    /// Retrieves the user-specified name for the given logging host name
+    /// User can give nicknames to "rename" logging hosts in the log, in case the host name provided by the device is inconvenient.
+    ///
+    /// - Parameter hostName: Logging host name provided by the log message
+    /// - Returns: Name user has chosen for this logging host, or the input string if no nickname was given
+    static func nickname(for hostName: String) -> String {
+        return hostNicknames[hostName] ?? hostName
+    }
+    
     
     /// Parse config from file
     ///
     /// - Parameter file: URL of the config file to read
     static func parse(_ file: URL) {
-        guard let str = try? String(contentsOf: file) else {
+        guard let configString = try? String(contentsOf: file) else {
             os_log(.error, log: cfgFileLogCtx, "Unable to read config file: %{public}", file.absoluteString)
             exit(-2)
         }
         
-        var searchPos = str.startIndex
-        while(true) {
-            // find the next log spec in between curly braces {...}
-            guard let specStart = str[searchPos...].range(of: "FORMAT") else { break }
-            guard let specEnd = str[specStart.upperBound...].range(of: "ENDFORMAT") else {
-                os_log(.error, log: cfgFileLogCtx, "Unmatched \"{\" in config file: %{public}", file.absoluteString)
-                exit(-3)
-            }
-            
-            // parse the log spec
-            guard let logSpec = Spec(from: str[str.index(after: specStart.upperBound)..<specEnd.lowerBound]) else {
-                os_log(.error, log: cfgFileLogCtx, "Malformed log spec in config file: %{public}", file.absoluteString)
-                exit(-4)
-            }
-            
-            // assign the log spec to the IP addresses on following lines
-            let hostEndPos = str[specEnd.upperBound...].range(of: "FORMAT")?.lowerBound ?? str.endIndex
-            
-            assign(spec: logSpec, toHosts: str[specEnd.upperBound..<hostEndPos])
-            searchPos = hostEndPos
+        let nicknameLine = Nodes([
+            StringNode(),
+            StringNode(),
+            ], onSuccess: { node in
+                guard let lvalue = node[0] else { return }
+                guard let rvalue = node[1] else { return }
+                self.assign(nickname: String(rvalue.text), to: String(lvalue.text))
+        })
+        
+        let nicknameBlock = Nodes([
+            Keyword("NICKNAMES", charCheck: keywordCharacters),
+            Char("{"),
+            Repeating(nicknameLine, separatedBy: nil),
+            Char("}"),
+        ])
+        
+        let formatBlock = Nodes([
+            Keyword("FORMAT", charCheck: keywordCharacters),
+            Char("{"),
+            Nodes([
+                Identifier(chars: { !$0.isWhitespace }),
+                Repeating(Options([
+                    Keyword("timestamp", charCheck: keywordCharacters),
+                    Keyword("host", charCheck: keywordCharacters),
+                    Keyword("category", charCheck: keywordCharacters),
+                    Keyword("message", charCheck: keywordCharacters),
+                ]), separatedBy: nil)
+            ]),
+            Char("}"),
+            Repeating(Options([
+                Char("*"),
+                IPAddrLiteralNode<IPv4Address>(),
+                IPAddrLiteralNode<IPv6Address>(),
+            ]), separatedBy: nil)
+            ], onSuccess: { node in
+                guard let node = node as? Nodes else { return }
+                guard let fmtNodes = node.nodes[2] as? Nodes else { return }
+                guard let hostNodes = node.nodes[4] as? Repeating else { return }
+                self.assign(format: fmtNodes, to: hostNodes)
+        })
+        
+        var confFileSyntax = Repeating(Options([
+            nicknameBlock,
+            formatBlock
+        ]), separatedBy: nil)
+        
+        var confSubstr = configString[...]
+        if !confFileSyntax.parse(from: &confSubstr) {
+            os_log(.info, log: cfgFileLogCtx, "Error parsing config file")
         }
+        confSubstr.dropWhitespace()
+        if confSubstr.count > 0 {
+            os_log(.error, log: cfgFileLogCtx, "Syntax error in config file starting from:\n%{public}s", String(confSubstr))
+        }
+    }
+    
+    /// Assigns the given nickname to the logging host name
+    /// - Parameters:
+    ///   - nickname: Nickname (name to appear in the log)
+    ///   - host: Actual host name as it appears in log messages
+    private static func assign(nickname: String, to host: String) {
+        hostNicknames[host] = nickname
     }
     
     /// Parses the host IP addresses following a log spec and assignes the given spec to those hosts
     /// - Parameters:
     ///   - spec: The preceeding log spec
     ///   - hostBlock: The block of text containing the host IP addresses
-    private static func assign(spec: Spec, toHosts hostBlock: Substring) {
-        let lines = hostBlock.split(separator: "\n")
+    private static func assign(format: Nodes, to hosts: Repeating) {
+        guard let regexNode = format.nodes[0] as? NonEmptyLineNode else { fatalError("Unexpected node type") }
+        guard let repeatingCaptures = format.nodes[1] as? Repeating else { fatalError("Unexpected node type") }
+        let regexPattern = String(regexNode.value)
+        
+        do {
+            let tmpRegex = try NSRegularExpression(pattern: regexPattern, options: [])
+        } catch {
+            fatalError("Unable to parse regex: \"\(regexPattern)\"")
+        }
+        
+        for node in format.nodes.dropFirst() {
+            guard let node = node as?
+        }
         
         for line in lines {
             let line = line.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
